d5a2a73111ca4630d7497d4543bf32dc34bb8e55
Patrick Dahl <patrick.v.d@hotmail.com>
2024-02-19 22:22:05 +0100

    Asteroids don't move in the same direction.
    Asteroids are no longer moving in same direction as each other.

--- Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroidsystem/AsteroidControlSystem.java
+++ Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroidsystem/AsteroidControlSystem.java
@@ -20,6 +20,7 @@ public class AsteroidControlSystem implements IEntityProcessingService {
         if (System.nanoTime() - lastAsteroidSpawn > asteroidCooldown) {
             System.out.println("process is running. Spawning should occur.");
             Asteroid asteroid = new Asteroid();
+            asteroid.setRotation(random.nextInt(360));
             asteroid.setPolygonCoordinates(-40,-40,80,0,-40,40);
             asteroidPlugin.createAsteroid(gameData);
             world.addEntity(asteroid);
