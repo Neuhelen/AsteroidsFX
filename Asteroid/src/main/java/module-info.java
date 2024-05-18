4fcab23c378c94b0c8be97e353441970367159a8
Neuhelen <patrick.v.d@hotmail.com>
2024-05-18 20:15:23 +0200

    Removed unnecessary dependency.

--- Asteroid/src/main/java/module-info.java
+++ Asteroid/src/main/java/module-info.java
@@ -3,7 +3,6 @@ import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 module Asteroid {
     requires Common;
-    requires CommonBullet;
     provides IGamePluginService with dk.sdu.mmmi.cbse.asteroidsystem.AsteroidPlugin;
     provides IEntityProcessingService with dk.sdu.mmmi.cbse.asteroidsystem.AsteroidControlSystem;
 }
