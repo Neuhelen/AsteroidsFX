458ca1b64b253a4ba644169b4eb94a60cb6ff8e8
Neuhelen <patrick.v.d@hotmail.com>
2024-05-18 20:56:10 +0200

    Removed the "PlayerControlSystem"'s dependency on "Player".

--- Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/Player.java
+++ Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/Player.java
@@ -7,13 +7,4 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
  * @author Emil
  */
 public class Player extends Entity {
-    private int lastShotTime = 0;
-
-    public int getLastShotTime() {
-        return lastShotTime;
-    }
-
-    public void setLastShotTime(int lastShotTime) {
-        this.lastShotTime = lastShotTime;
-    }
 }
